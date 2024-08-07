c760be3cfe7bd96c14552819e2dd9cdc78761a92
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-08-07 21:41:39 +0530

    chore: Disable prop type checking for the Employee component

---
+++ 2-Props/props-example/src/App.jsx
@@ -0,0 +1,14 @@
+import Employee from './Employees.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Employee name="John Doe" age={30}/>
+    </>
+  )
+}
+
+export default App
+
+// Johm doe is String and 30 is number
---
+++ 2-Props/props-example/src/Employees.jsx
@@ -0,0 +1,11 @@
+
+function Employee(props){
+    return(
+        <div className="div">
+            <p>Employee Name = {props.name}</p>
+            <p>Employee Age = {props.age}</p>
+        </div>
+    )
+}
+
+export default Employee;
