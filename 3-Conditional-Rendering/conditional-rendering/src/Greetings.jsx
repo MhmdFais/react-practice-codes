6d822f141ed8132226455d5a7d39cd763da48f49
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-08-10 12:37:52 +0530

    chore: Update Greetings component with class names for styling

--- 3-Conditional-Rendering/conditional-rendering/src/Greetings.jsx
+++ 3-Conditional-Rendering/conditional-rendering/src/Greetings.jsx
@@ -1,7 +1,7 @@
 function Greetings(props) {
     
     return (
-        props.isLoggedIn ? <h1>Welcome, {props.userName}!</h1> : <h1>Welcome, Guest!</h1>
+        props.isLoggedIn ? <h1 className="welcome-text">Welcome, {props.userName}!</h1> : <h1 className="login-text">Welcome, Guest!</h1>
     );
 
 }
