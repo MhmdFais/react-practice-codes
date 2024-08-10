52654cdf3d8c1e307eeafa4ab3721e2e4457ae99
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-08-10 12:19:17 +0530

    chore: Update default props for Employee component

--- 2-Props/props-example/src/Employees.jsx
+++ 2-Props/props-example/src/Employees.jsx
@@ -1,18 +1,17 @@
-
-function Employee(props){
-    return(
-        <div className="employeeCard">
-            <p>Employee Name = {props.name}</p>
-            <p>Employee Age = {props.age}</p>
-            <p>Employee is Manager = {props.isManager ? 'Yes' : 'No'}</p>
-        </div>
-    )
+function Employee(props) {
+  return (
+    <div className="employeeCard">
+      <p>Employee Name = {props.name}</p>
+      <p>Employee Age = {props.age}</p>
+      <p>Employee is Manager = {props.isManager ? "Yes" : "No"}</p>
+    </div>
+  );
 }
 
 Employee.defaultProps = {
-    name: 'Undefined',
-    age: 25,
-    isManager: false
-}
+  name: "Undefined",
+  age: 25,
+  isManager: false,
+};
 
 export default Employee;
