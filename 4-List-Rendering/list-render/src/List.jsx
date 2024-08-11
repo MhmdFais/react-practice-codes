4d2342b125407f01a131c700ae274cb83432e897
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-08-11 23:07:16 +0530

    chore: Add some array methods to List.jsx in 4-Rendr-List

--- 4-List-Rendering/list-render/src/List.jsx
+++ 4-List-Rendering/list-render/src/List.jsx
@@ -4,9 +4,23 @@ function List(){
         {name: 'Pizza', price: 200}, 
         {name: 'Biryani', price: 150}, 
         {name: 'Noodles', price: 120}, 
-        {name: 'Pasta', price: 180}
+        {name: 'Pasta', price: 180},
+        {name: 'Parotta', price: 50},
+        {name: 'Dosa', price: 40},
     ];
 
+    // sort by price
+    //foods.sort( (a, b) => a.price - b.price);
+
+    // sort by name
+    //foods.sort( (a, b) => a.name.localeCompare(b.name));
+
+    // filter by price > 100 and add a new array
+    const filteredFoods = foods.filter( (food) => food.price > 100);
+
+    const filteredFoodList = filteredFoods.map( (food, index) => <li key={index}>{food.name} - {food.price}</li>);
+
+    // ALL FOODS
     const foodList = foods.map( (food, index) => <li key={index}>{food.name} - {food.price}</li>);
 
     return(
@@ -14,6 +28,10 @@ function List(){
             <h1>Food Menu</h1>
             <ul>
                 {foodList}
+            </ul>
+            <h1>Foods which are above Rs.100 </h1>
+            <ul>
+                {filteredFoodList}
             </ul>
         </>
     );
