a09c06b3703207f5eb86beb0c26ae799b16daa93
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-08-12 19:34:09 +0530

    chore: Update Button component to handle multiple clicks and display a message

--- 5-Click-Event/click-event/src/Button.jsx
+++ 5-Click-Event/click-event/src/Button.jsx
@@ -1,10 +1,35 @@
 function Button(){
     
-    const handleClick = () => console.log('Button clicked');
+    //1
+    // const handleClick = () => console.log('Button clicked');
+
+    //2
+    // const handleClick = (name) => console.log(`Hello ${name}`);
+
+    //3
+    let clicks = 0
+    const handleClick = (name) => {
+        if (clicks <  3){
+            console.log(`${name} clicked me ${clicks+1} times`); 
+            clicks += 1;
+        }
+        else {
+            console.log(`Hello ${name} stop clicking me`);
+        }
+    };
 
     return(
-        <button className="btn" onClick={handleClick}>Click me</button>
+        //1
+        // <button className="btn" onClick={handleClick}>Click me</button>
+
+        //2
+        // <button className="btn" onClick={() => handleClick('John')}>Click me</button>
+
+        //3
+        <button className="btn" onClick={() => handleClick('John')}>Click me</button>
     );
 }
 
 export default Button;
+
+// ALSO "onDoubleClick" EVENT IS AVAILABLE
